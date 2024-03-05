32ca7070c483473c0ffdcac238e8ee2aa4c1fb4a
juliasiodaro <162338728+juliasiodaro@users.noreply.github.com>
2024-03-05 13:17:54 -0300

    Add files via upload

---
+++ exercicio_ac_1.py
@@ -0,0 +1,40 @@
+""" Julia Siodaro Engenharia de Software tarde
+AC 1 e 2
+
+"""
+# exercicio 1
+
+# ** 0,5
+
+def bhaskara_menos(a, b, c):
+    return(- b - (b ** 2 - (4 * a * c)) ** 0.5) / (2 * a)
+
+def bhaskara_mais(a, b, c):
+    return(- b + (b ** 2 - (4 * a * c)) ** 0.5) / (2 * a)
+
+def valores():
+    a = float(input("Informe o parâmetro a da equação: "))
+    b = float(input("Informe o parâmetro b da equação: "))
+    c = float(input("Informe o parâmetro c da equação: "))
+    print("A primeira raiz da equação é", bhaskara_menos(a, b, c))
+    print("A segunda raiz da equação é", bhaskara_mais(a, b, c))  
+      
+valores() 
+
+# exercicio 2
+
+def e_bi(ano):
+    bissexto = False
+    if ano%4 == 0 and ano%100 != 0:
+        bissexto = True
+    elif ano%400 == 0:
+        bissexto = True
+    return bissexto
+
+def ano():
+   ano = int(input("Informe o ano: "))
+   print("O ano é bissexto: ", e_bi(ano))
+
+ano()
+    
+
